344260e22c7236ec25c373c8c7cbf2d42df9a6f3
SupaFastBoi <gheocastefan497@gmail.com>
2024-05-03 15:00:51 +0300

    Finished int exercises

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,21 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+//      By default, length and width are set to 1 unit
+        Rectangle rectangle = new Rectangle(2 ,3);
+        rectangle.calculatePerimeter();
+
+//      By default, minutes are set to 0
+        Converter converter = new Converter(5);
+        converter.minutesToSeconds();
+
+//      By default, minutes are set to 1
+        Square square = new Square(10);
+        square.calculateArea();
     }
 }
---
+++ src/main/java/org/example/Rectangle.java
@@ -0,0 +1,53 @@
+package org.example;
+
+public class Rectangle {
+
+    //  declaring class fields
+    private int length;
+    private int width;
+
+    //  class constructors just for the sake of chaining
+    public Rectangle() {
+        this(1, 1);
+    }
+
+    public Rectangle(int length) {
+        this(length, 1);
+    }
+
+    public Rectangle(int length, int width) {
+        if (length < 0 || width < 0) {
+            throw new IllegalArgumentException("This type of field cannot hold a negative value");
+        }
+        this.length = length;
+        this.width = width;
+    }
+
+    //  getters and setters
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        if (width < 0) {
+            throw new IllegalArgumentException("This type of field cannot hold a negative value");
+        }
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        if (length < 0) {
+            throw new IllegalArgumentException("This type of field cannot hold a negative value");
+        }
+        this.length = length;
+    }
+
+    //  class methods
+    public void calculatePerimeter() {
+        System.out.println("Perimeter of Rectangle is: " + (getLength() + getWidth()));
+    }
+}
